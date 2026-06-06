82f80f1f90824630a331ef5a4e763e18a456a665
Darwin Gomez <darwintnt@hotmail.com>
2026-06-06 13:17:08 -0500

    feat(controller): implement TaskController with CRUD operations for tasks

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { PrismaTaskRepository } from "./infrastructure/repositories/prisma-task.repository.js";
+import { TaskController } from "./application/controllers/task.controller.js";
 
 dotenv.config();
 const port = process.env.PORT ?? 3000;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) =>
-  res.status(200).json({
-    message: "Hello World",
-  }),
+const repository = new PrismaTaskRepository();
+const taskController = new TaskController(repository);
+
+app.get("/tasks/:id", (req, res) => taskController.getById(req, res));
+app.get("/tasks", (req, res) => taskController.getAll(req, res));
+app.post("/tasks", (req, res) => taskController.create(req, res));
+app.put("/tasks/:id", (req, res) => taskController.update(req, res));
+app.patch("/tasks/:id/complete", (req, res) =>
+  taskController.complete(req, res),
 );
+app.delete("/tasks/:id", (req, res) => taskController.delete(req, res));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/infrastructure/repositories/prisma-task.repository.ts
+++ src/infrastructure/repositories/prisma-task.repository.ts
@@ -1,11 +1,21 @@
-import type { Task } from "../../../domain/entities/task.js";
-import type { ITaskRepository } from "../../../domain/repositories/task.repository.js";
+import { ITaskRepository } from "@/domain/repositories/task.repository.js";
+import { Task } from "@/domain/entities/task.js";
+import {
+  PrismaClient,
+  TaskSchema,
+} from "../database/generated/prisma/client.js";
+import { getPrismaClient } from "../database/prisma.client.js";
 
 export class PrismaTaskRepository implements ITaskRepository {
-  private repository: any;
+  private readonly repository: PrismaClient;
+
+  constructor() {
+    this.repository = getPrismaClient();
+  }
 
   async findAll(): Promise<Task[]> {
-    throw new Error("Method not implemented.");
+    const tasks = await this.repository.taskSchema.findMany();
+    return tasks.map((item: TaskSchema) => this.mapperToEntity(item));
   }
 
   async findById(id: string): Promise<Task | null> {
@@ -13,7 +23,16 @@ export class PrismaTaskRepository implements ITaskRepository {
   }
 
   async create(task: Task): Promise<Task> {
-    throw new Error("Method not implemented.");
+    console.log(task);
+
+    const newTask = await this.repository.taskSchema.create({
+      data: {
+        title: task.getTitle(),
+        completed: task.isCompleted(),
+      },
+    });
+
+    return this.mapperToEntity(newTask);
   }
 
   async update(task: Task): Promise<Task> {
@@ -22,5 +41,9 @@ export class PrismaTaskRepository implements ITaskRepository {
 
   async delete(id: string): Promise<void> {
     throw new Error("Method not implemented.");
+  }
+
+  private mapperToEntity(data: TaskSchema): Task {
+    return new Task(data.title, data.completed, data.id);
   }
 }
